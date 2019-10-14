6174ce84621a7225c46422717c9c6fd41cdcb54f
Sergey Belov <serge_belov@ukr.net>
2019-10-14 22:29:49 +0300

    Removed demo code.

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,5 @@ import 'package:flutter/material.dart';
 import 'package:trainer/app.dart';
 
 void main() {
-  var random = Random();
-  String s = '';
-  for (int i = 0; i < 100; ++i) {
-    s += random.nextInt(10).toString() + ' ';
-  }
-  print('s: $s');
-
   runApp(TrainerApp());
 }
