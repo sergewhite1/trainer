204603f2d8ecaccbae7ed5f8d8624eb4f022bc37
Sergey Belov <serge_belov@ukr.net>
2019-10-14 13:13:43 +0300

    On the way to MVVM.

---
+++ lib/ui/exercise_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class ExercisePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text('Trainer'),
+      ),
+      body: Container(
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          children: <Widget>[
+            Text(
+              'hello',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
