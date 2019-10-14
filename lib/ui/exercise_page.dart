0323de3640d83e13a58a05dc41dce85da76d0009
Sergey Belov <serge_belov@ukr.net>
2019-10-14 13:51:38 +0300

    Added "Next" button.

--- lib/ui/exercise_page.dart
+++ lib/ui/exercise_page.dart
@@ -12,23 +12,19 @@ class ExercisePage extends StatelessWidget {
       body: SafeArea(
         child: Container(
           child: Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Invoke "debug painting" (press "p" in the console, choose the
-            // "Toggle Debug Paint" action from the Flutter Inspector in Android
-            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-            // to see the wireframe for each widget.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               Text(
                 'hello',
+              ),
+              RaisedButton(
+                color: Colors.blue,
+                child: Text(
+                  'Next',
+                ),
+                onPressed: () {
+                  print('Next button pressed.');
+                },
               ),
             ],
           ),
