ffcaf96b0e1eceffb4166ff1b6511bd47627d1ea
Sergey Belov <serge_belov@ukr.net>
2019-10-14 21:17:44 +0300

    Skeleton for unit test of random generator.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:trainer/app.dart';
 
 void main() {
+  var random = Random();
+  String s = '';
+  for (int i = 0; i < 100; ++i) {
+    s += random.nextInt(10).toString() + ' ';
+  }
+  print('s: $s');
+
   runApp(TrainerApp());
 }
---
+++ test/random_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Random Test', () {
+    test('Test Case 1', () {
+      expect(0, 1);
+    });
+  });
+}
