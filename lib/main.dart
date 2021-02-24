a9f51808571bf971947969a8bad040dc1b47c457
Sergey Belov <serge_belov@ukr.net>
2021-02-24 20:58:32 +0200

    Migration to AndroidX.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'package:trainer/app.dart';
 import 'package:trainer/services/exercise_data_provider.dart';
 
 void main() async {
+  // Migration to AndroidX
+  WidgetsFlutterBinding.ensureInitialized();
+
   await ExerciseDataProvider.load();
   runApp(TrainerApp());
 }
